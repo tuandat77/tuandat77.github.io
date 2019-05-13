96999136a05e557a14b4eb3741174b26ea070ff1
unknown <tuandat7792@gmail.com>
2019-05-13 15:30:32 +0700

    a

--- 1.js
+++ 1.js
@@ -1,35 +1,31 @@
-﻿$(function(){
-
-//xác định vị trí của class so với vị trí top
-	console.log($('.top2').offset().top); //xác định vị trí của class top 2 so với vị trí top của web
-
-// cách làm cuộn chuột đến 1 vị trí trên web
- 
- 	$('.n1').click(function(){
- 		$('body').animate({scrollTop:$('.top6').offset().top});
- 		return false;
-	})
+﻿/*JS*/
+ document.addEventListener('DOMContentLoaded',function(){
+    var navbar = document.getElementById('navigation');
+    var status = 'under 50';
 
 
+     window.addEventListener('scroll',function(){
+        if (window.pageYOffset > 50){
+            if (status == 'under 50'){
+                status = 'over 50';
+                navbar.classList.add('bg');
+            }
+        }
+        else if ( window.pageYOffset <= 50) {
+            if ( status == 'over 50'){
+                status = 'under 50';
+                navbar.classList.remove('bg');
+            }
+        }
+    })
+})
 
-// xác định độ cao của màn hình
- 	var docao = $(window).height(); // hàm này để xem được độ cao của màn hình khi cho to hay cho nhỏ màn hình
-	console.log(docao); // hiện trên màn hình console.log độ cao của màn hình
+ /*Jquery*/
 
-	$('.top1').css({'height':docao});
-	$(window).resize(function() {
-		var docao = $(window).height();
-		$('.top1').css({'height':docao});
-	});
+$(document).ready(function () {
+	 $('.skill-information').slideUp();
+    $('._1line').click(function(){
+        $(this).children('.skill-information').slideToggle();
+    })
 
-	$('.nutmenu').click(function() {
-		$('.menuphai').addClass('ra');
-		return false; // đây là để chống lại khi sử dụng link cua the 'a' mà chỉ sử dụng xong khi sử dụng addclass
-	});
-
-	$('.tat').click(function() {
-		$('.menuphai').removeClass('ra'); 
-		return false; // đây là để chống lại khi sử dụng link cua the 'a' mà chỉ sử dụng xong khi sử dụng addclass
-	});
-})  
- 
+})
